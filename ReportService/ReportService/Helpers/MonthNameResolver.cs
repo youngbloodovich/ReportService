2364d85016d05b892e6fa4bd7758543453934099
Albert <dozhdeff@gmail.com>
2022-10-26 15:59:21 +0500

    Removed abusive library

---
+++ ReportService/ReportService/Helpers/MonthNameResolver.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System;
+
+namespace ReportService.Helpers
+{
+    public static class MonthNameResolver
+    {
+        public static string GetName(int year, int monthNum)
+        {
+            if (year <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year));
+            }
+
+            if (monthNum <= 0 || monthNum > 12)
+            {
+                throw new ArgumentOutOfRangeException(nameof(monthNum));
+            }
+
+            return new DateTime(year, monthNum, 1).ToString("MMMM", CultureInfo.CurrentCulture);
+        }
+    }
+}
