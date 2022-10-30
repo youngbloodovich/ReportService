ea61ed5acb003bf400c978eab54b2a2da824a8bb
Albert <dozhdeff@gmail.com>
2022-10-30 17:03:31 +0500

    + Formatter service was extended + Added unit-tests

--- ReportService/ReportService/Services/ReportFormatterService.cs
+++ ReportService/ReportService/Services/ReportFormatterService.cs
@@ -1,8 +1,9 @@
 ﻿using ReportService.Domain;
 using ReportService.Helpers;
-using ReportService.Repositories.Interfaces;
+using ReportService.Repositories.Entities;
 using ReportService.Services.Interfaces;
 using System;
+using System.Linq;
 using System.Text;
 
 namespace ReportService.Services
@@ -10,33 +11,47 @@ namespace ReportService.Services
     public class ReportFormatterService : IReportFormatterService
     {
         private const string Separator = "---";
+        private const int EmployeeNameLength = 50;
 
         private readonly StringBuilder _result = new StringBuilder();
 
         /// <inheritdoc/>
         public string Format(CompanyReport report)
         {
+            if (!report.DepartamentReports.Any())
+            {
+                return "В компании нет департаментов!";
+            }
+
             var monthName = MonthNameResolver.GetName(report.Month);
             _result.AppendLine($"{monthName} {report.Year}");
             _result.AppendLine(Environment.NewLine);
-
+            _result.AppendLine(Separator);
+            
             foreach (var departament in report.DepartamentReports)
             {
-                _result.AppendLine(Separator);
                 _result.AppendLine(departament.Name);
                 _result.AppendLine(Environment.NewLine);
 
+                if (!departament.EmployeeReports.Any())
+                {
+                    _result.AppendLine("В департаменте нет сотрудников!");
+                    _result.AppendLine(Environment.NewLine);
+                    _result.AppendLine(Separator);
+                    continue;
+                }
+
                 foreach (var employee in departament.EmployeeReports)
                 {
-                    _result.AppendLine($"{employee.Name} {employee.Salary}р");
+                    _result.AppendLine($"{employee.Name, -EmployeeNameLength}{employee.Salary}р");
                     _result.AppendLine(Environment.NewLine);
                 }
 
                 _result.AppendLine($"Всего по отделу {departament.Total}р");
                 _result.AppendLine(Environment.NewLine);
+                _result.AppendLine(Separator);
             }
-            
-            _result.AppendLine(Separator);
+
             _result.AppendLine(Environment.NewLine);
             _result.AppendLine($"Всего по предприятию {report.Total}р");
 
--- ReportServiceTests/ReportFormatterServiceTests.cs
+++ ReportServiceTests/ReportFormatterServiceTests.cs
@@ -1,7 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 using ReportService.Domain;
-using ReportService.Helpers;
 using ReportService.Repositories.Entities;
 using ReportService.Services;
 using System;
@@ -12,108 +10,130 @@ namespace ReportServiceTests
     public class ReportFormatterServiceTests
     {
         private const string Separator = "---";
+        private const int EmployeeNameLength = 50;
 
         [Test]
-        public void Format_GivenCompanyReport_WorksOk()
+        public void Format_GivenCorrectCompanyReport_WorksOk()
         {
+            var employee1 = new EmployeeReport { Name = "employee1", Salary = 70000 };
+            var employee2 = new EmployeeReport { Name = "employee2", Salary = 35000 };
+            var employee3 = new EmployeeReport { Name = "employee3", Salary = 45000 };
+
+            var departament1 = new DepartamentReport
+            {
+                Name = "departament1",
+                EmployeeReports = new EmployeeReport[] { employee1 }
+            };
+            var departament2 = new DepartamentReport
+            {
+                Name = "departament2",
+                EmployeeReports = new EmployeeReport[] { employee2, employee3 }
+            };
+
             var report = new CompanyReport
             {
-                Year = 2022,
-                Month = 2,
-                DepartamentReports = new DepartamentReport[]
-                {
-                    new DepartamentReport
-                    {
-                        Name = "departament1",
-                        EmployeeReports = new EmployeeReport[]
-                        {
-                            new EmployeeReport
-                            {
-                                Name = "employee1",
-                                Salary = 70000
-                            }
-                        }
-                    },
-                    new DepartamentReport
-                    {
-                        Name = "departament2",
-                        EmployeeReports = new EmployeeReport[]
-                        {
-                            new EmployeeReport
-                            {
-                                Name = "employee2",
-                                Salary = 70000
-                            },
-                            new EmployeeReport
-                            {
-                                Name = "employee3",
-                                Salary = 20000
-                            }
-                        }
-                    },
-                    new DepartamentReport
-                    {
-                        Name = "departament3",
-                        EmployeeReports = new EmployeeReport[]
-                        {
-                            new EmployeeReport
-                            {
-                                Name = "employee4",
-                                Salary = 50000
-                            },
-                            new EmployeeReport
-                            {
-                                Name = "employee5",
-                                Salary = 25000
-                            },
-                            new EmployeeReport
-                            {
-                                Name = "employee6",
-                                Salary = 65000
-                            }
-                        }
-                    }
-                }
+                Year = 2022, Month = 2,
+                DepartamentReports = new DepartamentReport[] { departament1, departament2 }
             };
 
             var expected = new StringBuilder();
             expected.AppendLine("Февраль 2022");
             expected.AppendLine(Environment.NewLine);
             expected.AppendLine(Separator);
-            expected.AppendLine("departament1");
+
+            expected.AppendLine(departament1.Name);
             expected.AppendLine(Environment.NewLine);
-            expected.AppendLine("employee1 70000р");
+            expected.AppendLine($"{employee1.Name, -EmployeeNameLength}{employee1.Salary}р");
             expected.AppendLine(Environment.NewLine);
             expected.AppendLine($"Всего по отделу 70000р");
             expected.AppendLine(Environment.NewLine);
             expected.AppendLine(Separator);
-            expected.AppendLine("departament2");
+
+            expected.AppendLine(departament2.Name);
             expected.AppendLine(Environment.NewLine);
-            expected.AppendLine("employee2 70000р");
+            expected.AppendLine($"{employee2.Name, -EmployeeNameLength}{employee2.Salary}р");
             expected.AppendLine(Environment.NewLine);
-            expected.AppendLine("employee3 20000р");
+            expected.AppendLine($"{employee3.Name, -EmployeeNameLength}{employee3.Salary}р");
             expected.AppendLine(Environment.NewLine);
-            expected.AppendLine($"Всего по отделу 90000р");
-            expected.AppendLine(Environment.NewLine);
-            expected.AppendLine(Separator);
-            expected.AppendLine("departament3");
-            expected.AppendLine(Environment.NewLine);
-            expected.AppendLine("employee4 50000р");
-            expected.AppendLine(Environment.NewLine);
-            expected.AppendLine("employee5 25000р");
-            expected.AppendLine(Environment.NewLine);
-            expected.AppendLine("employee6 65000р");
-            expected.AppendLine(Environment.NewLine);
-            expected.AppendLine($"Всего по отделу 140000р");
+            expected.AppendLine($"Всего по отделу 80000р");
             expected.AppendLine(Environment.NewLine);
             expected.AppendLine(Separator);
+
             expected.AppendLine(Environment.NewLine);
-            expected.AppendLine($"Всего по предприятию 300000р");
+            expected.AppendLine($"Всего по предприятию 150000р");
 
             var service = new ReportFormatterService();
             var actual = service.Format(report);
 
             Assert.AreEqual(expected.ToString(), actual);
+        }
+
+        [Test]
+        public void Format_GivenDeparmentWithoutEmployees_DisplaysCorrepondingMessage()
+        {
+            var employee1 = new EmployeeReport { Name = "employee1", Salary = 70000 };
+
+            var departament1 = new DepartamentReport
+            {
+                Name = "departament1",
+                EmployeeReports = new EmployeeReport[] { }
+            };
+            var departament2 = new DepartamentReport
+            {
+                Name = "departament2",
+                EmployeeReports = new EmployeeReport[] { employee1 }
+            };
+
+            var report = new CompanyReport
+            {
+                Year = 2022, Month = 2,
+                DepartamentReports = new DepartamentReport[] { departament1, departament2 }
+            };
+
+            var expected = new StringBuilder();
+            expected.AppendLine("Февраль 2022");
+            expected.AppendLine(Environment.NewLine);
+            expected.AppendLine(Separator);
+
+            expected.AppendLine(departament1.Name);
+            expected.AppendLine(Environment.NewLine);
+            expected.AppendLine("В департаменте нет сотрудников!");
+            expected.AppendLine(Environment.NewLine);
+            expected.AppendLine(Separator);
+
+            expected.AppendLine(departament2.Name);
+            expected.AppendLine(Environment.NewLine);
+            expected.AppendLine($"{employee1.Name,-EmployeeNameLength}{employee1.Salary}р");
+            expected.AppendLine(Environment.NewLine);
+            expected.AppendLine($"Всего по отделу 70000р");
+            expected.AppendLine(Environment.NewLine);
+            expected.AppendLine(Separator);
+
+            expected.AppendLine(Environment.NewLine);
+            expected.AppendLine($"Всего по предприятию 70000р");
+
+            var service = new ReportFormatterService();
+            var actual = service.Format(report);
+
+            Assert.AreEqual(expected.ToString(), actual);
+        }
+
+        [Test]
+        public void Format_GivenReportWithoutDepartments_DisplaysCorrepondingMessage()
+        {
+            var report = new CompanyReport
+            {
+                Year = 2022,
+                Month = 2,
+                DepartamentReports = new DepartamentReport[] { }
+            };
+
+            var expected = "В компании нет департаментов!";
+
+            var service = new ReportFormatterService();
+            var actual = service.Format(report);
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
